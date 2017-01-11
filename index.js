4739f226bf4ac667d7c4da1636dfe5135d3bbe3b
ph.charriere <ph.charriere@gmail.com>
2017-01-11 05:22:43 +0100

    :rocket: 1st version

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const port = process.env.PORT || 8080;
+
+const app = express();
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use(express.static(`./public`));
+
+app.get('/hello/world', (req, res) => {
+  res.send({from: "🐼", message: "hello 🌍"});
+})
+
+app.listen(port)
+console.log(`🌍 Web Server is started - listening on ${port}`)
